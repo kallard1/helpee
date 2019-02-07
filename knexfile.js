9a8186dd63a1dbd14c97bb5fde5dfca4cdc6dd66
kallard <contact@allard-kevin.fr>
2019-02-07 22:10:32 +0100

    config(knex): ajout de la configuration pour l'env test

--- knexfile.js
+++ knexfile.js
@@ -24,6 +24,25 @@ module.exports = {
     }
   },
 
+  test: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.join(__dirname, '/dist/migrations')
+    }
+  },
+
   production: {
     client: 'pg',
     connection: {
