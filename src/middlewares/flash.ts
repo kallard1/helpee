cdc1f7e7c48715e751b43385dc87408053d0ce34
kallard <contact@allard-kevin.fr>
2019-02-28 17:57:55 +0100

    Fix(flash): erreur du linter, ajout des conditions nécessaires

--- src/middlewares/flash.ts
+++ src/middlewares/flash.ts
@@ -1,19 +1,22 @@
 import { NextFunction, Request, Response } from "express";
-import { IFlashOptions } from "flash";
 
 export default function flash() {
   return (request: Request, response: Response, next: NextFunction) => {
-    if (request.session.flash) {
-      response.locals.flash = request.session.flash;
-      request.session.flash = undefined;
+    if (request.session) {
+      if (request.session.flash) {
+        response.locals.flash = request.session.flash;
+        request.session.flash = undefined;
+      }
     }
 
     request.flash = (type: string, content: string) => {
-      if (request.session.flash === undefined) {
-        request.session.flash = {};
-      }
+      if (request.session) {
+        if (request.session.flash === undefined) {
+          request.session.flash = {};
+        }
 
-      request.session.flash[type] = content;
+        request.session.flash = { type, message: content };
+      }
     };
 
     next();
