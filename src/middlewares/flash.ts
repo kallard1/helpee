5e8f16ad9c9e690eec94d0525b51b482ef60ba32
kallard <contact@allard-kevin.fr>
2019-02-28 10:41:05 +0100

    fix: révision du code

--- src/middlewares/flash.ts
+++ src/middlewares/flash.ts
@@ -1,52 +1,21 @@
-declare module "flash" {
-  import { NextFunction, Request, RequestHandler, Response } from "express";
-  import { format, isArray } from "util";
+import { NextFunction, Request, Response } from "express";
+import { IFlashOptions } from "flash";
 
-  interface IFlashOptions {
-    unsafe?: boolean;
-  }
-
-  export function flash(options?: IFlashOptions): RequestHandler {
-    options = options || {};
-
-    let safe: boolean;
-    safe = (options.unsafe === undefined) ? true : !options.unsafe;
-
-    return ((req: Request, res: Response, next: NextFunction) => {
-      if (req.flash && safe) {
-
-        return next();
-      }
-      req.flash = _flash;
-      next();
-    });
-  }
-
-  function _flash(type: string, msg: string): any {
-
-    if (this.session === undefined) throw Error("req.flash() requires sessions");
-
-    const msgs = this.session.flash = this.session.flash || {};
-
-    if (type && msg) {
-      if (arguments.length > 2 && format) {
-        const args = Array.prototype.slice.call(arguments, 1);
-        msg = format.apply(undefined, args);
-      } else if (isArray(msg)) {
-        msg.forEach((val) => {
-          (msgs[type] = msgs[type] || []).push(val);
-        });
-        return msgs[type].length;
-      }
-      return (msgs[type] = msgs[type] || []).push(msg);
-    }
-    if (type) {
-      const arr = msgs[type];
-      delete msgs[type];
-      return arr || [];
+export default function flash() {
+  return (request: Request, response: Response, next: NextFunction) => {
+    if (request.session.flash) {
+      response.locals.flash = request.session.flash;
+      request.session.flash = undefined;
     }
 
-    this.session.flash = {};
-    return msgs;
-  }
+    request.flash = (type: string, content: string) => {
+      if (request.session.flash === undefined) {
+        request.session.flash = {};
+      }
+
+      request.session.flash[type] = content;
+    };
+
+    next();
+  };
 }
