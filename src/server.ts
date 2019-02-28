499dc86b251d34b59c3068666da9e22253153afe
kallard <contact@allard-kevin.fr>
2019-02-28 10:39:29 +0100

    config: refonte de la configuration du serveur

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,57 @@
+import debug0 from "debug";
+import http from "http";
+
 import app from "./app";
 
-const server = app.listen(app.get("port"), () => {
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env"),
-  );
-  console.log("  Press CTRL-C to stop\n");
-});
+const debug = debug0("helpee:server");
+
+const port: number = app.get("port") || 3000;
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error: any) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string"
+    ? `Pipe ${port}`
+    : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string"
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
 
 export default server;
