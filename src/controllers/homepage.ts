16aad406ec807b7f4734582007617a47097966f7
kallard <contact@allard-kevin.fr>
2019-03-03 17:06:55 +0100

    fix: retrait debug

--- src/controllers/homepage.ts
+++ src/controllers/homepage.ts
@@ -2,8 +2,5 @@ import { Request, Response } from "express";
 import User from "../models/users";
 
 export let index = async (request: Request, response: Response) => {
-
-  console.log(await User.getAllUsers());
-
   response.render("homepage", { users: await User.getAllUsers() });
 };
