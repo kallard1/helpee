e67b2672c06f0138016dfebd60a372fcf1b004f8
kallard <contact@allard-kevin.fr>
2019-03-04 21:52:42 +0100

    :recycle: Refactoring code.

--- src/migrations/20190131234030_blog_posts.ts
+++ src/migrations/20190131234030_blog_posts.ts
@@ -1,42 +1,92 @@
 export let up = async (knex: any) => {
   await knex.schema.createTable("blog_categories", (table: any) => {
-    table.uuid("uuid").notNull().defaultTo(knex.raw("uuid_generate_v4()"));
-    table.string("label", 75).notNullable();
-    table.dateTime("created_at").defaultTo(knex.fn.now()).notNullable();
-    table.dateTime("updated_at").defaultTo(knex.fn.now()).nullable();
+    table.uuid("uuid")
+      .notNull()
+      .defaultTo(knex.raw("uuid_generate_v4()"));
+    table.string("label", 75)
+      .notNullable();
+    table.dateTime("created_at")
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table.dateTime("updated_at")
+      .defaultTo(knex.fn.now())
+      .nullable();
     table.primary(["uuid"]);
   });
 
   await knex.schema.createTable("blog_posts", (table: any) => {
-    table.uuid("uuid").notNull().defaultTo(knex.raw("uuid_generate_v4()"));
-    table.uuid("blog_category_uuid").notNullable();
-    table.string("title", 95).notNullable();
-    table.text("content").notNullable();
-    table.boolean("is_published").nullable().defaultTo(true);
-    table.dateTime("published_at").nullable();
-    table.boolean("is_deleted").nullable().defaultTo(false);
-    table.boolean("can_comment").nullable().defaultTo(true);
-    table.uuid("user_uuid").notNullable();
-    table.dateTime("created_at").defaultTo(knex.fn.now()).notNullable();
-    table.dateTime("updated_at").defaultTo(knex.fn.now()).nullable();
+    table.uuid("uuid")
+      .notNull()
+      .defaultTo(knex.raw("uuid_generate_v4()"));
+    table.uuid("blog_category_uuid")
+      .notNullable();
+    table.string("title", 95)
+      .notNullable();
+    table.text("content")
+      .notNullable();
+    table.boolean("is_published")
+      .nullable()
+      .defaultTo(true);
+    table.dateTime("published_at")
+      .nullable();
+    table.boolean("is_deleted")
+      .nullable()
+      .defaultTo(false);
+    table.boolean("can_comment")
+      .nullable()
+      .defaultTo(true);
+    table.uuid("user_uuid")
+      .notNullable();
+    table.dateTime("created_at")
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table.dateTime("updated_at")
+      .defaultTo(knex.fn.now())
+      .nullable();
     table.primary(["uuid"]);
     table.index(["blog_category_uuid", "user_uuid"]);
-    table.foreign("blog_category_uuid").references("uuid").inTable("blog_categories").onDelete("CASCADE").onUpdate("CASCADE");
-    table.foreign("user_uuid").references("uuid").inTable("users").onDelete("CASCADE").onUpdate("CASCADE");
+    table.foreign("blog_category_uuid")
+      .references("uuid")
+      .inTable("blog_categories")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table.foreign("user_uuid")
+      .references("uuid")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
   });
 
   await knex.schema.createTable("blog_comments", (table: any) => {
-    table.uuid("uuid").notNull().defaultTo(knex.raw("uuid_generate_v4()"));
-    table.uuid("user_uuid").notNullable();
-    table.uuid("blog_post_uuid").notNullable();
+    table.uuid("uuid")
+      .notNull().
+    defaultTo(knex.raw("uuid_generate_v4()"));
+    table.uuid("user_uuid")
+      .notNullable();
+    table.uuid("blog_post_uuid")
+      .notNullable();
     table.text("content").notNullable();
-    table.boolean("is_published").nullable().defaultTo(true);
-    table.dateTime("created_at").defaultTo(knex.fn.now()).notNullable();
-    table.dateTime("updated_at").defaultTo(knex.fn.now()).nullable();
+    table.boolean("is_published")
+      .nullable()
+      .defaultTo(true);
+    table.dateTime("created_at")
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table.dateTime("updated_at")
+      .defaultTo(knex.fn.now())
+      .nullable();
     table.primary(["uuid"]);
     table.index(["user_uuid", "blog_post_uuid"]);
-    table.foreign("user_uuid").references("uuid").inTable("users").onDelete("CASCADE").onUpdate("CASCADE");
-    table.foreign("blog_post_uuid").references("uuid").inTable("blog_posts").onDelete("CASCADE").onUpdate("CASCADE");
+    table.foreign("user_uuid")
+      .references("uuid")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table.foreign("blog_post_uuid")
+      .references("uuid")
+      .inTable("blog_posts")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
   });
 };
 
