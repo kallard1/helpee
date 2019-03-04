5c82b6f2af9d8f5bf0da497b2fb00e46d2324b93
kallard <contact@allard-kevin.fr>
2019-03-04 21:53:49 +0100

    tests: Rechargement de la base de test avant l'execution des tests
    Il est mieux de repartir sur des bases saines

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,20 @@
 import request from "supertest";
 
 import app from "../app";
+import bdd from "../config/bdd";
 
 describe("GET /", () => {
+
+  beforeAll((done) => {
+    bdd.knex.migrate.rollback()
+      .then(() => {
+        bdd.knex.migrate.latest()
+          .then(() => {
+            done();
+          });
+      });
+  });
+
   it("should return 200", async () => {
     const response = await request(app)
       .get("/")
