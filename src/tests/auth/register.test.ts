7f37e44c8b880ff92d32d42c8f8d45cc10597bde
kallard <contact@allard-kevin.fr>
2019-03-04 21:59:27 +0100

    tests: modification du jeu de test + optimisations

--- src/tests/auth/register.test.ts
+++ src/tests/auth/register.test.ts
@@ -1,12 +1,14 @@
-import supertest from "supertest";
-import app from "../../app";
+import request from "supertest";
 
-const request = supertest(app);
+import app from "../../app";
 
 describe("GET /auth/register", () => {
   it("should return 200", async () => {
-    const response = await request.get("/auth/register");
+    const response = await request(app)
+      .get("/auth/register")
+      .send();
 
-    expect(response.status).toEqual(200);
+    expect(response.status).toBe(200);
+    expect(response.header["content-type"]).toEqual("text/html; charset=utf-8");
   });
 });
