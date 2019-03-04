343df354edfa4d53f1bea0a97a9f9931cdc0e1b3
kallard <contact@allard-kevin.fr>
2019-03-04 19:23:48 +0100

    perf: optimisation du code

--- src/config/bdd.ts
+++ src/config/bdd.ts
@@ -6,12 +6,6 @@ if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
 
-const host: string = (process.env.DATABASE_HOST !== undefined ? process.env.DATABASE_HOST : "127.0.0.1");
-const port: string = (process.env.DATABASE_PORT !== undefined ? process.env.DATABASE_PORT : "5432");
-const username: string = (process.env.DATABASE_USER !== undefined ? process.env.DATABASE_USER : "");
-const password: string = (process.env.DATABASE_PASSWORD !== undefined ? process.env.DATABASE_PASSWORD : "");
-const name: string = (process.env.DATABASE_NAME !== undefined ? process.env.DATABASE_NAME : "");
-
 class Bdd {
   public client: Client;
   public knex: knex;
@@ -21,11 +15,11 @@ class Bdd {
     this.knex = knex({
       client: "pg",
       connection: {
-        host: host,
-        port: port,
-        user: username,
-        password: password,
-        database: name,
+        host: process.env.DATABASE_HOST || "127.0.0.1",
+        port: process.env.DATABASE_PORT || "5432",
+        user: process.env.DATABASE_USER || "",
+        password: process.env.DATABASE_PASSWORD || "",
+        database: process.env.DATABASE_NAME || "",
       },
       pool: {
         min: 2,
