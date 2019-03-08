9cedd411f4d4acd6b693ee040e8fee1ea4c42943
kallard <contact@allard-kevin.fr>
2019-03-08 16:31:29 +0100

    fix(homepage): suppression code

--- src/controllers/homepage.ts
+++ src/controllers/homepage.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
-import User from "../models/users";
 
 export let index = async (request: Request, response: Response) => {
-  response.render("homepage", { users: await User.getAllUsers() });
+  response.render("homepage");
 };
