04fcf71604a780a604ba502e7f0145ad52d517a1
kallard <contact@allard-kevin.fr>
2019-03-09 21:03:21 +0100

    feature(routes): modification du comportement des routes
    ajout du controle du formulaire (règles de validation)

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,54 @@
-import csrf from "csurf";
 import express from "express";
+import { check } from "express-validator/check";
 
 import * as registerController from "../controllers/auth/register";
+import { default as User } from "../models/user";
 
 const index = express.Router();
-const csrfProtection = csrf({ cookie: true });
 
-index.get("/register", csrfProtection, registerController.index);
-index.post("/register", csrfProtection, registerController.registration);
+index.get("/register", registerController.index);
+index.post("/register",
+  [
+    check("firstname")
+      .isLength({ max: 75 }).withMessage("Firstname is too long")
+      .exists().withMessage("Firstname is required"),
+    check("lastname")
+      .isLength({ max: 75 }).withMessage("Lastname is too long")
+      .exists().withMessage("Lastname is required"),
+    check("password")
+      .exists().withMessage("Password is required")
+      .isLength({ min: 8 }).withMessage("Password must need 8 characters min.")
+      .matches(/[0-9]/).withMessage("Password must contain at least 1 number.")
+      .matches(/[a-z]/).withMessage("Password must contain at least 1 lowercase letter.")
+      .matches(/[A-Z]/).withMessage("Password must contain at least 1 uppercase letter."),
+    check("passwordConfirmation")
+      .exists().withMessage("Password confirmation is required")
+      .isLength({ min: 8 }).withMessage("Password must need 8 characters min.")
+      .matches(/[0-9]/).withMessage("Password must contain at least 1 number.")
+      .matches(/[a-z]/).withMessage("Password must contain at least 1 lowercase letter.")
+      .matches(/[A-Z]/).withMessage("Password must contain at least 1 uppercase letter.")
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Password confirmation does not match password");
+        }
+
+        return true;
+      }),
+    check("email").isEmail().custom((email) => {
+      return User.findOne({ email }).then((user): any => {
+        if (user) {
+          return Promise.reject("E-mail already in use");
+        }
+      });
+    }).normalizeEmail({ gmail_remove_dots: false }),
+    check("address").isLength({ max: 75 }),
+    check("address1").isLength({ max: 75 }),
+    check("zipCode").isPostalCode("FR"),
+    check("city").exists().withMessage("City is required"),
+    check("phone")
+      .isLength({ min: 10, max: 12 }).withMessage("Phone number must contain 10 characters.")
+      .matches(/((\+?)(33|0)?([0-9]{9}))/g).withMessage("Invalid format for phone number."),
+  ],
+  registerController.registration);
 
 export default index;
