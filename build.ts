d116d5baf75ce0c759eeab3f1294cbfc24a17bcb
kallard <contact@allard-kevin.fr>
2019-03-09 21:42:51 +0100

    build: ajout de suppression du dossier dist avant le build

---
+++ build.ts
@@ -0,0 +1,7 @@
+import path from "path";
+import s from "shelljs";
+const config = require(path.join(`${__dirname}/tsconfig.json`));
+
+const outDir = config.compilerOptions.outDir;
+
+s.rm("-rf", outDir);
