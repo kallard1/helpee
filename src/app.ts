8d7c3886e7a8819bd3ca36fa4fe2a041a559fa24
kallard <contact@allard-kevin.fr>
2019-03-10 11:37:50 +0100

    perf: réécriture de la configuration du serveur

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ import compression from "compression";
 import cookieParser from "cookie-parser";
 import csurf from "csurf";
 import express, { NextFunction, Request, Response } from "express";
-import expressSession from "express-session";
+import session from "express-session";
 import expressValidator from "express-validator";
 import createError from "http-errors";
+import mongoose from "mongoose";
 import logger from "morgan";
 import path from "path";
 
@@ -13,86 +14,109 @@ import winston from "./config/winston";
 import flash from "./middlewares/flash";
 
 import authRouter from "./routes/auth";
-import indexRouter from "./routes/index";
+import rootRouter from "./routes/root";
 
-const app = express();
+class App {
+  public express: express.Application;
 
-// Session configuration
-const session: any = {
-  secret: process.env.SECRET_KEY || "",
-  name: "helpee_session",
-  cookie: {
-    expires: new Date(Date.now() + 60 * 60 * 1000),
-  },
-  resave: false,
-  saveUninitialized: true,
-};
+  constructor() {
+    this.express = express();
+    this.middleware();
+    this.routes();
+    this.launchConf();
+  }
 
-if (process.env.NODE_ENV === "production") {
-  app.set("trust proxy", 1);
-  session.cookie = {
-    secure: true,
-    httpOnly: true,
-    domain: "helpee.fr",
-  };
+  private middleware(): void {
+    this.express.set("port", process.env.PORT || 3000);
+
+    this.express.set("views", path.join(__dirname, "../views"));
+    this.express.set("view engine", "ejs");
+
+    if (process.env.NODE_ENV === "production") {
+      this.express.set("trust proxy", 1);
+    }
+
+    this.express.use(compression());
+    this.express.use(logger("combined", { stream: winston.stream }));
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: true }));
+    this.express.use(expressValidator());
+    this.express.use(cookieParser());
+    this.express.use(session({
+      secret: process.env.SECRET_KEY || "",
+      name: "helpee_session",
+      cookie: {
+        expires: new Date(Date.now() + 60 * 60 * 1000),
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: process.env.NODE_ENV === "production",
+        domain: process.env.NODE_ENV === "production" ? "helpee.fr" : "",
+      },
+      resave: false,
+      saveUninitialized: true,
+    }));
+
+    this.express.use(flash());
+    this.express.use(csurf({ cookie: true }));
+
+    this.express.use("*", (req, res, next) => {
+      res.locals.csrfToken = req.csrfToken();
+      next();
+    });
+
+    this.express.use(express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 }));
+
+  }
+
+  private routes(): void {
+    this.express.use("/", rootRouter);
+    this.express.use("/auth/", authRouter);
+  }
+
+  private launchConf() {
+    mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost:27017/database", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    mongoose.connection.on("error", () => {
+      // tslint:disable-next-line:no-console
+      console.log("MongoDB connection error. Please make sure MongoDB is running.");
+      process.exit();
+    });
+
+    this.express.use((req: Request, res: Response, next: NextFunction) => {
+      next(createError(404));
+    });
+
+    this.express.use((err: any, req: Request, res: Response, next: NextFunction) => {
+      if (err.code !== "EBADCSRFTOKEN") return next(err);
+
+      // handle CSRF token errors here
+      res.status(403);
+      res.render("error", {
+        message: "CODE RED - NO CSRF TOKEN",
+        error: {},
+      });
+
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get("env") === "development" ? err : {};
+
+      winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render("error");
+    });
+
+    this.express.listen(this.express.get("port"), () => {
+      // tslint:disable-next-line:no-console
+      console.log(("  App is running at http://localhost:%d \
+      in %s mode"), this.express.get("port"), this.express.get("env"));
+      // tslint:disable-next-line:no-console
+      console.log("  Press CTRL-C to stop\n");
+    });
+  }
 }
 
-// Express configuration
-app.set("port", process.env.PORT || 3000);
-
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "ejs");
-
-app.use(logger("combined", { stream: winston.stream }));
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
-app.use(cookieParser());
-app.use(
-  express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 }),
-);
-app.use(expressSession(session));
-
-if (process.env.NODE_ENV === "test") {
-  app.use(csurf({ ignoreMethods: ["GET", "HEAD", "OPTIONS", "POST", "PUT" /* etc */] }));
-} else {
-  app.use(csurf({ cookie: true }));
-}
-app.use(flash());
-
-app.get("*", (req, res, next) => {
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
-
-app.use("/", indexRouter);
-app.use("/auth/", authRouter);
-
-// catch 404 and forward to error handler
-app.use((req: Request, res: Response, next: NextFunction) => {
-  next(createError(404));
-});
-
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err.code !== "EBADCSRFTOKEN") return next(err);
-
-  // handle CSRF token errors here
-  res.status(403);
-  res.render("error", {
-    message: "CODE RED - NO CSRF TOKEN",
-    error: {},
-  });
-
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-export default app;
+export default new App().express;
