a95cfa98e1e5dbba5017571f3aba892a67caede0
kallard <contact@allard-kevin.fr>
2019-03-12 22:10:13 +0100

    feat: ajout de l'envoi du mail si le mot de passe est perdu

--- src/config/email.ts
+++ src/config/email.ts
@@ -3,7 +3,7 @@ import { UserModel } from "../models/user";
 
 SGmail.setApiKey("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");
 
-function newUserEmail(user: UserModel) {
+export function newUserEmail(user: UserModel) {
   const message = {
     to: user.email,
     from: {
@@ -21,4 +21,29 @@ function newUserEmail(user: UserModel) {
     });
 }
 
-export { newUserEmail };
+export function forgotPassword(user: UserModel, done: any) {
+  const message = {
+    to: user.email,
+    from: {
+      email: "no-reply@helpee.fr",
+      name: "No-Reply Helpee",
+    },
+    subject: "Helpee: Forgot password ",
+    text: `Hi ${user.firstname} ${user.lastname},\n
+    You are receiving this because you (or someone else) have requested the reset of the password for your account.\n
+    Please click on the following link, or paste this into your browser to complete the process:\n\n
+    https://www.helpee.fr/auth/forgot-password/${user.resetPasswordToken}\n\n
+    If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    html: `Hi ${user.firstname} ${user.lastname},\n
+    You are receiving this because you (or someone else) have requested the reset of the password for your account.\n
+    Please click on the following link, or paste this into your browser to complete the process:\n\n
+    http://www.helpee.fr/auth/forgot-password/${user.resetPasswordToken}\n\n
+    If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+  };
+
+  SGmail.send(message).then(() => {
+    done(null, "done");
+  }).catch((err) => {
+    return console.error(err);
+  });
+}
