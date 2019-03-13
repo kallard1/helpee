1362c6f124f14c58d85c4952a8d49028a655d924
kallard <contact@allard-kevin.fr>
2019-03-13 17:22:09 +0100

    feat: ajout de l'email de confirmation du changement de mot de passe

--- src/config/email.ts
+++ src/config/email.ts
@@ -28,7 +28,7 @@ export function forgotPassword(user: UserModel, done: any) {
       email: "no-reply@helpee.fr",
       name: "No-Reply Helpee",
     },
-    subject: "Helpee: Forgot password ",
+    subject: "Helpee: Forgot password",
     text: `Hi ${user.firstname} ${user.lastname},\n
     You are receiving this because you (or someone else) have requested the reset of the password for your account.\n
     Please click on the following link, or paste this into your browser to complete the process:\n\n
@@ -39,6 +39,27 @@ export function forgotPassword(user: UserModel, done: any) {
     Please click on the following link, or paste this into your browser to complete the process:\n\n
     http://www.helpee.fr/auth/reset-password/${user.resetPasswordToken}\n\n
     If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+  };
+
+  SGmail.send(message).then(() => {
+    done(null, "done");
+  }).catch((err) => {
+    return console.error(err);
+  });
+}
+
+export function editPasswordConfirmation(user: UserModel, done: any) {
+  const message = {
+    to: user.email,
+    from: {
+      email: "no-reply@helpee.fr",
+      name: "No-Reply Helpee",
+    },
+    subject: "Helpee: Your password has been changed!",
+    text: `Hi ${user.firstname} ${user.lastname},\n
+    This is a confirmation that the password for your account has just been changed.\n`,
+    html: `Hi ${user.firstname} ${user.lastname},\n
+    This is a confirmation that the password for your account has just been changed.`,
   };
 
   SGmail.send(message).then(() => {
