3422aa1677328bb4b34687785309227e3f8b4155
kallard <contact@allard-kevin.fr>
2019-03-19 11:10:24 +0100

    fix: clean pour windows

--- build.ts
+++ build.ts
@@ -1,8 +1,15 @@
+import fs from "fs";
 import path from "path";
 import s from "shelljs";
+
 const config = require(path.join(`${__dirname}/tsconfig.json`));
 
 const outDir = config.compilerOptions.outDir;
 
-s.rm("-rf", outDir);
-s.rm("-rf", "./public/build");
+if (process.platform !== "win32") {
+  s.rm("-rf", outDir);
+  s.rm("-rf", "./public/build");
+} else {
+  fs.rmdir(outDir, () => {});
+  fs.rmdir("./public/build", () => {});
+}
