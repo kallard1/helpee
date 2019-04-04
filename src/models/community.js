4edc529099979589b213b70baf494d5c9b97bbb3
kallard <contact@allard-kevin.fr>
2019-04-04 18:33:20 +0200

    feat: création du modèle

---
+++ src/models/community.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 75,
+      trim: true
+    },
+    slug: {
+      type: String,
+      required: true,
+      maxlength: 100,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    user: {
+      type: mongoose.Schema.User.ObjectId,
+      ref: 'User'
+    },
+    members: [{
+      type: mongoose.Schema.User.ObjectId,
+      ref: 'User'
+    }],
+    is_enabled: {
+      type: Boolean,
+      default: true
+    },
+    zip_code: {
+      type: String,
+      required: true,
+      maxlength: 5,
+      trim: true
+    },
+    city: {
+      type: String,
+      required: true,
+      maxlength: 75,
+      trim: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Community = mongoose.model('Community', schema);
+
+module.exports = Community;
