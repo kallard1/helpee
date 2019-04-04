f11b3bbcc742488403ab672da0426f74d25f2866
kallard <contact@allard-kevin.fr>
2019-04-04 12:09:09 +0200

    fix: modification de la méthode de slugification

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,18 +1,11 @@
-class Slugify {
+const slugify = (string) => {
+  let value = string;
+  value = value.toLowerCase();
+  value = value.normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '');
+  value = value.replace(/[^a-z0-9]+/ig, '-');
 
-  /**
-   * Slugify string
-   * @param string
-   */
-  replace(string) {
-    let value = string;
-    value = value.toLowerCase();
-    value = value.normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '');
-    value = value.replace(/[^a-z0-9]+/ig, '-');
+  return value;
+};
 
-    return value;
-  }
-}
-
-module.exports = Slugify;
+module.exports = slugify;
