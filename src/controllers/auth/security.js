2208b28f89cd05983300792be39db7d97a2ed285
kallard <contact@allard-kevin.fr>
2019-04-08 09:47:18 +0200

    fix: réinclusion de la fonctionnalité de réinitialisation du mot de passe

--- src/controllers/auth/security.js
+++ src/controllers/auth/security.js
@@ -1,7 +1,12 @@
+import async from 'async';
 import passport from 'passport';
+import { v4 as uuid } from 'uuid';
+import { forgotPassword } from '../../config/email';
+
+import User from '../../models/user';
 
 /**
- * GET Login page.
+ * GET /auth/login.
  *
  * @param req
  * @param res
@@ -18,7 +23,7 @@ exports.index = async(req, res) => {
 };
 
 /**
- * POST Login page.
+ * POST /auth/login.
  *
  * @param req
  * @param res
@@ -33,7 +38,82 @@ exports.login = async(req, res, next) => {
 };
 
 /**
- * GET Logout page.
+ * GET /auth/forgot-password
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+exports.forgot = async(req, res) => {
+  res.render('auth/forgot/forgot');
+};
+
+/**
+ * POST /auth/forgot-password
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<void>}
+ */
+exports.generateToken = async(req, res, next) => {
+  async.waterfall([
+    (done) => {
+      const token = uuid();
+
+      done(null, token);
+    },
+    (token, done) => {
+      User.findOne({ email: req.body.email }, (err, user) => {
+        const u = user;
+        if (!user) {
+          req.flash('warning', 'No account found with this email.');
+          return res.redirect('/auth/forgot-password');
+        }
+
+        if (err) {
+          console.error(err);
+        }
+
+        u.resetPasswordToken = token;
+        u.resetPasswordExpires = Date.now() + 3600000;
+
+        u.save((error) => {
+          done(error, u);
+        });
+      });
+    },
+    (user, done) => {
+      forgotPassword(user, done);
+    }
+  ], (err) => {
+    if (err) return next(err);
+    req.flash('info', 'An e-mail has been sent with further instructions.');
+    return res.redirect('/auth/forgot-password');
+  });
+};
+
+/**
+ * /auth/reset-password/:token
+ */
+exports.resetPassword = async(req, res) => {
+  User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  }, (err, user) => {
+    if (!user) {
+      req.flash('danger', 'Password reset token is invalid or has expired');
+      return res.redirect('/auth/forgot-password');
+    }
+
+    if (err) {
+      console.error(err);
+    }
+
+    res.render('auth/forgot/reset');
+  });
+};
+
+/**
+ * GET /auth/logout.
  *
  * @param req
  * @param res
