781057a2faab7de86bc1b565d388a5712b8c98ec
kallard <contact@allard-kevin.fr>
2019-04-08 10:02:38 +0200

    style: reorganisation du code

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,15 +12,18 @@ router.get('/register', registerController.index);
 router.post('/register',
   [
     check('firstname')
-      .isLength({ max: 75 }).withMessage('Firstname is too long')
+      .isLength({ max: 75 })
+      .withMessage('Firstname is too long')
       .exists()
       .withMessage('Firstname is required'),
     check('lastname')
-      .isLength({ max: 75 }).withMessage('Lastname is too long')
+      .isLength({ max: 75 })
+      .withMessage('Lastname is too long')
       .exists()
       .withMessage('Lastname is required'),
     check('password')
-      .exists().withMessage('Password is required')
+      .exists()
+      .withMessage('Password is required')
       .isLength({ min: 8 })
       .withMessage('Password must need 8 characters min.')
       .matches(/[0-9]/)
@@ -30,7 +33,8 @@ router.post('/register',
       .matches(/[A-Z]/)
       .withMessage('Password must contain at least 1 uppercase letter.'),
     check('passwordConfirmation')
-      .exists().withMessage('Password confirmation is required')
+      .exists()
+      .withMessage('Password confirmation is required')
       .isLength({ min: 8 })
       .withMessage('Password must need 8 characters min.')
       .matches(/[0-9]/)
@@ -46,17 +50,29 @@ router.post('/register',
 
         return true;
       }),
-    check('email').isEmail().custom(email => User.findOne({ email }).then((user) => {
-      if (user) {
-        return Promise.reject(new Error('E-mail already in use'));
-      }
-    })).normalizeEmail({ gmail_remove_dots: false }),
-    check('address').isLength({ max: 75 }),
-    check('address1').isLength({ max: 75 }),
-    check('zipCode').isPostalCode('FR'),
-    check('city').exists().withMessage('City is required'),
+    check('email')
+      .isEmail()
+      .custom(email => User.findOne({ email })
+        .then((user) => {
+          if (user) {
+            return Promise.reject(new Error('E-mail already in use'));
+          }
+        }))
+      .normalizeEmail({ gmail_remove_dots: false }),
+    check('address')
+      .isLength({ max: 75 }),
+    check('address1')
+      .isLength({ max: 75 }),
+    check('zipCode')
+      .isPostalCode('FR'),
+    check('city')
+      .exists()
+      .withMessage('City is required'),
     check('phone')
-      .isLength({ min: 10, max: 12 })
+      .isLength({
+        min: 10,
+        max: 12
+      })
       .withMessage('Phone number must contain 10 characters.')
       .matches(/((\+?)(33|0)?([0-9]{9}))/g)
       .withMessage('Invalid format for phone number.')
