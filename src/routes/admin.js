611278b66ac83eab52bfaacb45d3dabbbcc2b8da
THOMAS  JACQUART <JACQUART@GROUPE.AD.ETAM>
2019-04-15 14:37:08 +0200

    feat: integration page confirmation

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,7 +4,7 @@ import express from 'express';
 import { isLoggedInAndAdmin } from '../middlewares/isLoggedIn';
 
 import * as adsCategoriesController from '../controllers/admin/ads/ads_categories';
-import * as dashboardController from '../controllers/admin/dashboard';
+//import * as dashboardController from '../controllers/admin/dashboard';
 
 const router = express.Router();
 
