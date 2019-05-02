8696a7a3cfb8058d2b1bce5d938dc0f7ba667f0b
kallard <contact@allard-kevin.fr>
2019-05-02 13:03:34 +0200

    config: ajout de l'import des données

---
+++ docker/mongodb/docker-entrypoint-initdb.d/ads_categories.js
@@ -0,0 +1,119 @@
+db.ads_categories.insert({
+  'label': 'Voiture',
+  'slug': 'voiture'
+});
+
+db.ads_categories.insert({
+  'label': 'Motos',
+  'slug': 'motos'
+});
+
+db.ads_categories.insert({
+  'label': 'Utilitaires',
+  'slug': 'utilitaires'
+});
+
+db.ads_categories.insert({
+  'label': 'Vélos',
+  'slug': 'velos'
+});
+
+db.ads_categories.insert({
+  'label': 'Informatique',
+  'slug': 'informatique'
+});
+
+db.ads_categories.insert({
+  'label': 'Consoles & jeux',
+  'slug': 'consoles-jeux'
+});
+
+db.ads_categories.insert({
+  'label': 'Image & son',
+  'slug': 'image-son'
+});
+
+db.ads_categories.insert({
+  'label': 'Électroménager',
+  'slug': 'electromenager'
+});
+
+db.ads_categories.insert({
+  'label': 'Jardinage',
+  'slug': 'jardinage'
+});
+
+db.ads_categories.insert({
+  'label': 'Bricolage',
+  'slug': 'bricolage'
+});
+
+db.ads_categories.insert({
+  'label': 'Vêtements',
+  'slug': 'vetements'
+});
+
+db.ads_categories.insert({
+  'label': 'Équipement bébé',
+  'slug': 'equipement-bebe'
+});
+
+db.ads_categories.insert({
+  'label': 'Vêtements bébé',
+    'slug': 'vetements-bebe'
+});
+
+db.ads_categories.insert({
+  'label': 'DVD / Film',
+  'slug': 'dvd-film'
+});
+
+db.ads_categories.insert({
+  'label': 'CD / Musique',
+  'slug': 'cd-musique'
+});
+
+db.ads_categories.insert({
+  'label': 'Livres',
+  'slug': 'livres'
+});
+
+db.ads_categories.insert({
+  'label': 'Animaux',
+  'slug': 'animaux'
+});
+
+db.ads_categories.insert({
+  'label': 'Jeux & jouets',
+  'slug': 'jeux-jouets'
+});
+
+db.ads_categories.insert({
+  'label': 'Prestation de services',
+  'slug': 'prestation-services'
+});
+
+db.ads_categories.insert({
+  'label': 'Cours particulier',
+  'slug': 'cours-particulier'
+});
+
+db.ads_categories.insert({
+  'label': 'Co-voiturage',
+  'slug': 'co-voiturage'
+});
+
+db.ads_categories.insert({
+  'label': 'Outillage',
+  'slug': 'outillage'
+});
+
+db.ads_categories.insert({
+  'label': 'Sport',
+  'slug': 'sport'
+});
+
+db.ads_categories.insert({
+  'label': 'Autre',
+  'slug': 'autre'
+});
