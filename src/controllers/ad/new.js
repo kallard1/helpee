6ca59b23d61ab12b7eb3adfe04a35c84b9dc8042
kallard <contact@allard-kevin.fr>
2019-05-08 14:41:32 +0200

    feat: affichage de la communauté de l'utilisateur s'il en a plusieurs

--- src/controllers/ad/new.js
+++ src/controllers/ad/new.js
@@ -1,4 +1,5 @@
 import AdsCategories from '../../models/ad/category';
+import Community from '../../models/community';
 import Departments from '../../models/department';
 
 /**
@@ -10,6 +11,8 @@ import Departments from '../../models/department';
 exports.index = async(req, res) => {
   res.render('ad/new', {
     categories: await AdsCategories.find(),
-    departments: await Departments.find()
+    departments: await Departments.find(),
+    // eslint-disable-next-line no-underscore-dangle
+    communities: await Community.find({ members: req.user._id })
   });
 };
