cbdf940b42ef8c66c4f3da5075184de53b9b2858
kallard <contact@allard-kevin.fr>
2019-05-10 18:13:47 +0200

    feat: refonte de la liste des catégories

--- docker/mongodb/docker-entrypoint-initdb.d/ads_categories.js
+++ docker/mongodb/docker-entrypoint-initdb.d/ads_categories.js
@@ -1,119 +1,153 @@
-db.ads_categories.insert({
-  'label': 'Voiture',
-  'slug': 'voiture'
-});
+db.ads_categories.drop();
 
-db.ads_categories.insert({
-  'label': 'Motos',
-  'slug': 'motos'
-});
-
-db.ads_categories.insert({
-  'label': 'Utilitaires',
-  'slug': 'utilitaires'
-});
-
-db.ads_categories.insert({
-  'label': 'Vélos',
-  'slug': 'velos'
-});
-
-db.ads_categories.insert({
-  'label': 'Informatique',
-  'slug': 'informatique'
-});
-
-db.ads_categories.insert({
-  'label': 'Consoles & jeux',
-  'slug': 'consoles-jeux'
-});
-
-db.ads_categories.insert({
-  'label': 'Image & son',
-  'slug': 'image-son'
-});
-
-db.ads_categories.insert({
-  'label': 'Électroménager',
-  'slug': 'electromenager'
-});
-
-db.ads_categories.insert({
-  'label': 'Jardinage',
-  'slug': 'jardinage'
-});
-
-db.ads_categories.insert({
-  'label': 'Bricolage',
-  'slug': 'bricolage'
-});
-
-db.ads_categories.insert({
+db.ads_categories.insertMany([{
+  'label': 'Véhicules',
+  'slug': 'vehicules',
+  'categories': [
+    {
+      'label': 'Voiture',
+      'slug': 'voiture'
+    }, {
+      'label': 'Moto',
+      'slug': 'moto'
+    },
+    {
+      'label': 'Utilitaires',
+      'slug': 'utilitaires'
+    }
+  ]
+}, {
+  'label': 'Loisirs',
+  'slug': 'loisirs',
+  'categories': [
+    {
+      'label': 'Vélo',
+      'slug': 'velo'
+    },
+    {
+      'label': 'DVD / Films',
+      'slug': 'dvd-films'
+    },
+    {
+      'label': 'CD / Musique',
+      'slug': 'cd-musique'
+    },
+    {
+      'label': 'Jeux & Jouets',
+      'slug': 'jeux-jouets'
+    },
+    {
+      'label': 'Livres',
+      'slug': 'livres'
+    },
+    {
+      'label': 'Animaux',
+      'slug': 'animaux'
+    },
+    {
+      'label': 'Sport',
+      'slug': 'sport'
+    }
+  ]
+}, {
+  'label': 'Multimédia',
+  'slug': 'multimedia',
+  'categories': [
+    {
+      'label': 'Informatique',
+      'slug' : 'informatique'
+    },
+    {
+      'label': 'Consoles & Jeux vidéos',
+      'slug' : 'consoles-jeux-videos'
+    },
+    {
+      'label': 'Image & Son',
+      'slug' : 'image-son'
+    },
+    {
+      'label': 'Téléphonie',
+      'slug' : 'telephonie'
+    }
+  ]
+}, {
+  'label': 'Maison',
+  'slug': 'maison',
+  'categories': [
+    {
+      'label': 'Electroménager',
+      'slug' : 'electromenager'
+    },
+    {
+      'label': 'Jardinage',
+      'slug' : 'jardinage'
+    },
+    {
+      'label': 'Bricolage',
+      'slug' : 'bricolage'
+    },
+    {
+      'label': 'Ameublement',
+      'slug' : 'ameublement'
+    }
+  ]
+}, {
+  'label': 'Services',
+  'slug': 'services',
+  'categories': [
+    {
+      'label': 'Prestations de service',
+      'slug' : 'prestations-service'
+    },
+    {
+      'label': 'Cours particuliers',
+      'slug' : 'cours-particuliers'
+    },
+    {
+      'label': 'Covoiturage',
+      'slug' : 'covoiturage'
+    },
+    {
+      'label': "Garde d'enfants",
+      'slug' : 'garde-enfants'
+    }
+  ]
+}, {
   'label': 'Vêtements',
-  'slug': 'vetements'
-});
-
-db.ads_categories.insert({
-  'label': 'Équipement bébé',
-  'slug': 'equipement-bebe'
-});
-
-db.ads_categories.insert({
-  'label': 'Vêtements bébé',
-    'slug': 'vetements-bebe'
-});
-
-db.ads_categories.insert({
-  'label': 'DVD / Film',
-  'slug': 'dvd-film'
-});
-
-db.ads_categories.insert({
-  'label': 'CD / Musique',
-  'slug': 'cd-musique'
-});
-
-db.ads_categories.insert({
-  'label': 'Livres',
-  'slug': 'livres'
-});
-
-db.ads_categories.insert({
-  'label': 'Animaux',
-  'slug': 'animaux'
-});
-
-db.ads_categories.insert({
-  'label': 'Jeux & jouets',
-  'slug': 'jeux-jouets'
-});
-
-db.ads_categories.insert({
-  'label': 'Prestation de services',
-  'slug': 'prestation-services'
-});
-
-db.ads_categories.insert({
-  'label': 'Cours particulier',
-  'slug': 'cours-particulier'
-});
-
-db.ads_categories.insert({
-  'label': 'Co-voiturage',
-  'slug': 'co-voiturage'
-});
-
-db.ads_categories.insert({
-  'label': 'Outillage',
-  'slug': 'outillage'
-});
-
-db.ads_categories.insert({
-  'label': 'Sport',
-  'slug': 'sport'
-});
-
-db.ads_categories.insert({
-  'label': 'Autre',
-  'slug': 'autre'
-});
+  'slug': 'vetements',
+  'categories': [
+    {
+      'label': 'Homme',
+      'slug' : 'homme'
+    },
+    {
+      'label': 'Femme',
+      'slug' : 'femme'
+    },
+    {
+      'label': 'Enfant',
+      'slug' : 'enfant'
+    },
+    {
+      'label': 'Maternité',
+      'slug' : 'maternite'
+    }
+  ]
+}, {
+  'label': 'Univers bébé',
+  'slug': 'univers-bebe',
+  'categories': [
+    {
+      'label': 'Équipement bébé',
+      'slug' : 'equipement-bebe'
+    },
+    {
+      'label': 'Vêtements bébé',
+      'slug' : 'vetements-bebe'
+    }
+  ]
+}, {
+  'label': 'Autres',
+  'slug': 'autres'
+}
+]);
