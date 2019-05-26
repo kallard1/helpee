6141e4fe9ec7f3b93e672a5440600af0cc10622f
kallard <contact@allard-kevin.fr>
2019-05-26 22:54:28 +0200

    style: prettier

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -14,13 +14,18 @@ router.get('/ads/category/new', isLoggedInAndAdmin, adsCategoriesController.new)
 
 router.get('/ads/category/edit/:slug', isLoggedInAndAdmin, adsCategoriesController.edit);
 
-router.post('/ads/category/save/:id?', isLoggedInAndAdmin, [
-  check('label')
-    .isLength({ max: 75 })
-    .withMessage('Label is too long')
-    .exists()
-    .withMessage('Label is required')
-], adsCategoriesController.save);
+router.post(
+  '/ads/category/save/:id?',
+  isLoggedInAndAdmin,
+  [
+    check('label')
+      .isLength({ max: 75 })
+      .withMessage('Label is too long')
+      .exists()
+      .withMessage('Label is required')
+  ],
+  adsCategoriesController.save
+);
 router.get('/ads/category/delete/:slug', isLoggedInAndAdmin, adsCategoriesController.delete);
 
 module.exports = router;
