ff42401c3a9ac50c4c38716698cf261fa7339148
kallard <contact@allard-kevin.fr>
2019-05-26 22:54:52 +0200

    feat: ajout sluggifier

--- src/models/community.js
+++ src/models/community.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import slug from 'mongoose-slug-updater';
+
+mongoose.plugin(slug);
 
 const schema = new mongoose.Schema(
   {
@@ -10,9 +13,11 @@ const schema = new mongoose.Schema(
     },
     slug: {
       type: String,
+      slug: 'name',
       required: true,
       maxlength: 100,
-      trim: true
+      trim: true,
+      unique: true
     },
     description: {
       type: String,
