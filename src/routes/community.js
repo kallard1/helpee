844afc4c3428b324bb930b30dab914bdb56f8c51
kallard <contact@allard-kevin.fr>
2019-05-26 22:54:14 +0200

    feat: ajout route

--- src/routes/community.js
+++ src/routes/community.js
@@ -7,18 +7,25 @@ import { isLoggedIn } from '../middlewares/isLoggedIn';
 const router = express.Router();
 
 router.get('/new', isLoggedIn, communityController.new);
-router.post('/save', isLoggedIn, [
-  check('name')
-    .isLength({ max: 75 })
-    .withMessage('Name is too long')
-    .exists()
-    .withMessage('Name is required'),
-  check('city')
-    .exists()
-    .withMessage('City is required'),
-  check('description')
-    .exists()
-    .withMessage('Description is required')
-], communityController.save);
+router.post(
+  '/save',
+  isLoggedIn,
+  [
+    check('name')
+      .isLength({ max: 75 })
+      .withMessage('Name is too long')
+      .exists()
+      .withMessage('Name is required'),
+    check('city')
+      .exists()
+      .withMessage('City is required'),
+    check('description')
+      .exists()
+      .withMessage('Description is required')
+  ],
+  communityController.save
+);
+
+router.get('/:slug', communityController.getBySlug);
 
 module.exports = router;
